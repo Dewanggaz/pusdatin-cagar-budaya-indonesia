3488f66a03b51d4333212f0b1c2dbbb52ffc61d9
Dewanggaz <zidandewangga252@gmail.com>
2026-08-05 14:31:33 +0700

    fix: ignore dist & node_modules, remove stale build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pusdatin-cagar-budaya-indonesia/",
+  
   server: {
     port: 5173,
     open: true,
