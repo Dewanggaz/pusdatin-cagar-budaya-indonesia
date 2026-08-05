889ba968a1ada417e9bb5cfb22bede834a946d4e
Dewanggaz <zidandewangga252@gmail.com>
2026-08-05 13:41:59 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pusdatin-budaya/",
+  base: "/pusdatin-cagar-budaya-indonesia/",
   server: {
     port: 5173,
     open: true,
   },
-}); 
+});
